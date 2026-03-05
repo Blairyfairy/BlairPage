e2d7f69e195c9cbd983363fbd36133a4b5906a28
Blair Page <blarghtrill@gmail.com>
2026-03-04 17:10:43 -0800

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,21 @@ themeToggle.addEventListener("click", () => {
     themeToggle.textContent = "🌙";
   }
 });
+
+// Scroll To Top Button
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+});
+
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
